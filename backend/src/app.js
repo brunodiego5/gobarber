f273550f40ebae8006b9eff1406b30526758d4ff
brunodiego6 <bruno_diego5@hotmail.com>
2020-01-29 22:54:06 -0300

    inicio do curso gostack gobarber

---
+++ backend/src/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import routes from './routes';
+
+class App {
+  //chamado automáticamente quanto App instânciado 
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  //todos os middlewares do app
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+//exportar apenas o server
+export default new App().server;
