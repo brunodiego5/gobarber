ae5424a2eb3b90b78cefba9ef399a406629daa48
brunodiego6 <bruno_diego5@hotmail.com>
2020-02-06 23:33:04 -0300

    router upload avatar, add multer

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,17 +1,23 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddlewares from './app/middlewares/auth';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddlewares);
-/* todas as rotas a baixo passaram pelo middlewares authMiddlewares */
+/* todas as rotas abaixo passaram pelo middlewares authMiddlewares */
 
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
