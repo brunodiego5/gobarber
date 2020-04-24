43235a00dee92418d8e26a9715a11a05848c6984
Bruno Domingues <bruno_diego5@hotmail.com>
2020-04-24 20:26:10 -0300

    04 - Configurando Reactotron

--- web/src/App.js
+++ web/src/App.js
@@ -3,6 +3,7 @@ import { Router } from 'react-router-dom';
 
 import Routes from './routes';
 import history from './services/history';
+import './config/ReactotronConfig';
 
 function App() {
   return (
