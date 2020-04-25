196b8d2eb52dbcfc777d4b99f5391fd7ae4bb2a4
brunodiego5 <bruno_diego5@hotmail.com>
2020-04-25 00:43:12 -0300

    05 - Rotas privadas

--- web/src/pages/Dashboard/index.js
+++ web/src/pages/Dashboard/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 // import { Container } from './styles';
 
-export default function Dashboard() {
-  return <div />;
+export default function () {
+  return <h1>Dashboard</h1>;
 }
