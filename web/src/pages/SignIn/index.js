58fc7ccc7edd98f771cc704307d23ef79b0f6929
brunodiego5 <bruno_diego5@hotmail.com>
2020-04-25 23:37:46 -0300

    10 - Utilizando Unform

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
+  function hnaldeSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
 
-      <form>
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Seu senha secreta" />
+      <Form onSubmit={hnaldeSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Seu senha secreta"
+        />
 
         <button type="submit">Acessar</button>
         <Link to="/register">Criar conta gratuita</Link>
-      </form>
+      </Form>
     </>
   );
 }
