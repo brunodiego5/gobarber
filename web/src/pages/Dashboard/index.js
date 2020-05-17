9a46c9d19b75a717e96ddb4c27266924f6b842f8
Bruno Domingues <bruno_diego5@hotmail.com>
2020-05-17 17:02:47 -0300

    28 - Estilização do Dashboard

--- web/src/pages/Dashboard/index.js
+++ web/src/pages/Dashboard/index.js
@@ -1,7 +1,40 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import * as Styled from './styles';
 
 export default function () {
-  return <h1>Dashboard</h1>;
+  return (
+    <Styled.Container>
+      <Styled.Header>
+        <Styled.Button type="button">
+          <Styled.IconLeft size={36} color="#fff" />
+        </Styled.Button>
+
+        <Styled.Strong>31 de Maio</Styled.Strong>
+
+        <Styled.Button type="button">
+          <Styled.IconRight size={36} color="#fff" />
+        </Styled.Button>
+      </Styled.Header>
+
+      <Styled.List>
+        <Styled.Time past>
+          <Styled.StrongTime>08:00</Styled.StrongTime>
+          <Styled.Span>Bruno Domingues</Styled.Span>
+        </Styled.Time>
+        <Styled.Time>
+          <Styled.StrongTime available>09:00</Styled.StrongTime>
+          <Styled.Span available>Em aberto</Styled.Span>
+        </Styled.Time>
+        <Styled.Time>
+          <Styled.StrongTime>10:00</Styled.StrongTime>
+          <Styled.Span>Bruno Domingues</Styled.Span>
+        </Styled.Time>
+        <Styled.Time>
+          <Styled.StrongTime>11:00</Styled.StrongTime>
+          <Styled.Span>Bruno Domingues</Styled.Span>
+        </Styled.Time>
+      </Styled.List>
+    </Styled.Container>
+  );
 }
