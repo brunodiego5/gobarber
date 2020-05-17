ad9137fc7bbb0e8c9c8a2168365b331ec343aacc
Bruno Domingues <bruno_diego5@hotmail.com>
2020-05-17 13:57:41 -0300

    23 - Página de perfil

--- web/src/pages/Profile/index.js
+++ web/src/pages/Profile/index.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-// import { Container } from './styles';
+import * as Styled from './styles';
 
 export default function Profile() {
-  return <div />;
+  const profile = useSelector((state) => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Styled.Container>
+      <Styled.FormProfile initialData={profile} onSubmit={handleSubmit}>
+        <Styled.InputProfile name="name" placeholder="Nome completo" />
+        <Styled.InputProfile
+          name="email"
+          placeholder="Seu endereço de e-email"
+        />
+
+        <Styled.Hr />
+
+        <Styled.InputProfile
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Styled.InputProfile
+          type="password"
+          name="password"
+          placeholder="Nova senha"
+        />
+        <Styled.InputProfile
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação de senha"
+        />
+        <Styled.Button type="submit">Atualizar Perfil</Styled.Button>
+      </Styled.FormProfile>
+
+      <Styled.ButtonSair type="button">Sair do GoBarber</Styled.ButtonSair>
+    </Styled.Container>
+  );
 }
